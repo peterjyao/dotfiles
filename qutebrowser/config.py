94f565f4ee13bb8ff667e8993803cbfe9400d252
Yao Peter - Basel <peter.yao@lonza.com>
2021-06-28 22:22:16 +0200

    misc changes to git, qutebrowser, vim

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -506,7 +506,7 @@ c.window.transparent = False
 
 # Default zoom level.
 # Type: Perc
-c.zoom.default = '110%'
+c.zoom.default = '100%'
 
 # Available zoom levels.
 # Type: List of Perc
@@ -786,7 +786,7 @@ c.colors.webpage.bg = 'white'
 # `colors.webpage.darkmode.threshold.background` to 205.  - "With
 # selective inversion of everything": Combines the two variants   above.
 # Type: Bool
-c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.enabled = False
 
 # Which algorithm to use for modifying how colors are rendered with
 # darkmode. The `lightness-cielab` value was added with QtWebEngine 5.14
